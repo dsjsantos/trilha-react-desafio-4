a09db9a7815d828d158aa2023d61ad2a7c6d7749
Daniel Jegorschki Santos <dssantos@gmail.com>
2024-05-18 18:12:07 -0300

    Corigito bug de tipo em IButtonProps

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import { IButtonProps } from './types';
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<IButtonProps>`
     width: 100%;
     height: 42px;
     background-color: #81259D;
